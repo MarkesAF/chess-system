149b18dc58d9c72f3bb9231560de724a477290ac
Augusto Marques <markesaf16@gmail.com>
2023-08-03 18:22:51 -0300

    move pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,13 +5,26 @@ import chess.ChessMatch;
 import java.util.Locale;
 import java.util.Scanner;
 
-
+import chess.ChessPosition;
 import chess.ChessPiece;
 import chess.ChessMatch;
 
 public class Program {
     public static void main(String[]args){
+        Scanner in = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+
+        while(true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(in);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(in);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source,target);
+        }
     }
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -5,6 +5,7 @@ import chess.Color;
 import chess.pieces.Rook;
 import chess.pieces.King;
 import boardgame.Position;
+import boardgame.Piece;
 public class ChessMatch {
     private Board board;
 
@@ -20,6 +21,24 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+    public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition){
+        Position source = sourcePosition.toPosition();
+        Position target = targetPosition.toPosition();
+        validadeSourcePosition(source);
+        Piece capturedPiece = makeMove(source, target);
+        return (ChessPiece) capturedPiece;
+    }
+    private void validadeSourcePosition(Position position){
+        if(!board.thereIsAPiece(position)){
+            throw new ChessException("There is no piece on source position");
+        }
+    }
+    private Piece makeMove(Position source, Position target){
+        Piece p = board.removePiece(source);
+        Piece capturedPiece = board.removePiece(target);
+        board.placePiece( p,target);
+        return capturedPiece;
     }
     private void placeNewPiece(char column, int row, ChessPiece piece){
         board.placePiece(piece,new ChessPosition(column,row).toPosition());
@@ -38,8 +57,5 @@ public class ChessMatch {
         placeNewPiece('e', 7, new Rook(board, Color.BLACK));
         placeNewPiece('e', 8, new Rook(board, Color.BLACK));
         placeNewPiece('d', 8, new King(board, Color.BLACK));
-
-
     }
-    
 }
